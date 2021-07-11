5899c01d32e8d0e7f1d280a9c71fff6e46729ff4
Lucas Silva <lucas@brainyresolution.com>
2021-07-11 20:41:36 -0300

    feature: added basic routing modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
 import Login from "./pages/login";
+import SignUp from "./pages/signup";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/signup" component={SignUp}></Route>
+        <Route exact path="/" component={Login}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,37 @@
 import React from "react";
 
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
+
 export default function Home() {
+  let match = useRouteMatch();
+
   return (
     <div>
-      <h1> Hello , user</h1>
+      <BrowserRouter>
+        <nav>
+          <Link to="/home">Home page</Link>
+          <Link to="/help">Help</Link>
+        </nav>
+
+        <Switch>
+          <Route path={match.url}>
+            <section>
+              <h1> Hello , user</h1>
+            </section>
+          </Route>
+          <Route path="/help">
+            <section>
+              <h1>Help</h1>
+            </section>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
