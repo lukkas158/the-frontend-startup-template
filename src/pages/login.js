e4714b7174dadfafea4e326c5c7b5f442a3bcb04
Lucas Silva <lucas@brainyresolution.com>
2021-07-13 20:31:04 -0300

    feature(login) login with token

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,18 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import api from "~/services/api";
+import { userSelector } from "~/store/reducers/users";
 
-export default function Login() {
+export function Login(props) {
+  const { isLogged } = useSelector(userSelector);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState("");
+  const [error, setError] = useState("");
+  const history = useHistory();
+
+  if (isLogged) {
+    history.push("/home");
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+    setError("");
+
+    try {
+      let response = await api.post("/token/", { email, password });
+      localStorage.setItem("access", response.data.access);
+
+      history.push("/home");
+    } catch (error) {
+      setError("Unable to login in");
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <div>
       <h1> Login </h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <label> Email:</label>
-        <input type="email" />
-        <input type="password" />
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          type="email"
+        />
+        <input
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          type="password"
+        />
         <label> Password </label>
-        <button type="submit"> Login </button>
-        <a href="#"> Esqueceu sua senha ?</a>
-        <a href="#">Cadastrar</a>
+        <button disabled={loading} type="submit">
+          Login
+        </button>
+        {error}
+        <a href="/login"> Forgot your password ?</a>
+        <Link to="/signup">Sign up</Link>
       </form>
     </div>
   );
 }
+
+export default Login;
